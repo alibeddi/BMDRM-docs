2b8998a48a28953ce97fddeeb428d73467a70009
alibeddi <ali7beddi@gmail.com>
2026-01-08 14:46:53 +0100

    chore: configure base path for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'BMDRM-docs';
+
 const nextConfig = {
   output: 'export',
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
   images: {
     unoptimized: true,
   },
